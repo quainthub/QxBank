e2513ad046a0f9147f9f9f09d318ee59dffc9e15
Quaint Xu <quaint_xu@yahoo.com>
2025-02-03 15:46:29 -0600

    Initial commit. New qx-bank project

---
+++ src/test/java/com/quaint/qx_bank/QxBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quaint.qx_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QxBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
