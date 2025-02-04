c65d9831ab6cab9d27932d1adaf12d1ea7ff2fec
Quaint Xu <quaint_xu@yahoo.com>
2025-02-04 00:07:04 -0600

    Add controller

---
+++ src/main/java/com/quaint/qx_bank/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.quaint.qx_bank.controller;
+
+import com.quaint.qx_bank.dto.BankResponse;
+import com.quaint.qx_bank.dto.UserRequest;
+import com.quaint.qx_bank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public BankResponse createAccount(@RequestBody UserRequest userRequest){
+        return userService.createAccount(userRequest);
+    }
+}
